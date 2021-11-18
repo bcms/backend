4e6f4c4ea7785066bf06c0e00dede536f84b61c0
Bbanez <banezprod@gmail.com>
2021-11-18 13:17:22 +0100

    Add skip option

--- src/util/type-converter.ts
+++ src/util/type-converter.ts
@@ -9,12 +9,14 @@ export class BCMSTypeConverter {
   static async typescript({
     target,
     type,
+    skip,
   }: {
     target: BCMSGroup | BCMSWidget | BCMSTemplate;
     type: 'group' | 'template' | 'widget';
+    skip?: string[];
   }): Promise<BCMSTypeConverterResultItem[]> {
     // eslint-disable-next-line no-console
-    console.log(type, target);
+    console.log({ type, target, skip });
     return [];
   }
 }
