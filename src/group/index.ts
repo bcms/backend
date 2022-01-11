ae9b2447bf6b3dd9043a4d0dd69c05db10afa037
Luka <lukagrujic80@gmail.com>
2022-01-11 11:50:39 +0100

    fix index group

--- src/group/index.ts
+++ src/group/index.ts
@@ -1,4 +1,9 @@
+export * from './inputs';
+export * from './objects';
+export * from './resolvers';
+
 export * from './collection';
 export * from './controller';
 export * from './factory';
 export * from './repository';
+export * from './request-handler';
