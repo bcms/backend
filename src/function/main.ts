8da582d71c5e942e9792054e4a90701311a39cff
Bbanez <banezprod@gmail.com>
2022-01-13 11:47:29 +0100

    Add api function cleanup on startup

--- src/function/main.ts
+++ src/function/main.ts
@@ -10,85 +10,105 @@ import {
   BCMSFunctionManager,
   BCMSFunctionSchema,
 } from '../types';
+import { BCMSRepo } from '@bcms/repo';
 
 let functionManager: BCMSFunctionManager;
+
+async function init() {
+  let fns: BCMSFunction<unknown>[] = [];
+  const fs = useFS();
+  const objectUtil = useObjectUtility();
+  const stringUtil = useStringUtility();
+  const fnsPath = path.join(process.cwd(), 'functions');
+  if (await fs.exist(fnsPath)) {
+    const fnNames = await fs.readdir(fnsPath);
+    for (let i = 0; i < fnNames.length; i++) {
+      const fnName = fnNames[i];
+      if (
+        fnName.endsWith('.js') ||
+        (!fnName.endsWith('.d.ts') && fnName.endsWith('.ts'))
+      ) {
+        const fnImport: {
+          default: () => Promise<BCMSFunction<unknown>>;
+        } = await import(path.join(fnsPath, fnName));
+        const checkFn = objectUtil.compareWithSchema(
+          { fn: fnImport.default },
+          {
+            fn: {
+              __type: 'function',
+              __required: true,
+            },
+          },
+          fnName,
+        );
+        if (checkFn instanceof ObjectUtilityError) {
+          throw Error(checkFn.message);
+        }
+        const fn = await fnImport.default();
+        const checkObject = objectUtil.compareWithSchema(
+          fn,
+          BCMSFunctionSchema,
+          fnName,
+        );
+        if (checkObject instanceof ObjectUtilityError) {
+          throw Error(checkObject.message);
+        }
+        fn.config.name = stringUtil.toSlug(fn.config.name);
+        if (fns.find((e) => e.config.name === fn.config.name)) {
+          throw Error(
+            `Duplicate of "${fn.config.name}" function.` +
+              ' This is not allowed.',
+          );
+        }
+        fns.push(fn);
+      }
+    }
+  }
+  functionManager = {
+    clear() {
+      fns = [];
+    },
+    get(name) {
+      return fns.find((e) => e.config.name === name);
+    },
+    getAll() {
+      return fns;
+    },
+  };
+
+  /**
+   * Fix API Keys
+   */
+  {
+    const apiKeys = await BCMSRepo.apiKey.findAll();
+    for (let i = 0; i < apiKeys.length; i++) {
+      const apiKey = apiKeys[i];
+      let changes = false;
+      let j = 0;
+      while (j < apiKey.access.functions.length) {
+        const fnName = apiKey.access.functions[j];
+        const fnAvailable = functionManager.get(fnName.name);
+        if (!fnAvailable || fnAvailable.config.public) {
+          changes = true;
+          apiKey.access.functions.splice(j, 1);
+        } else {
+          j++;
+        }
+      }
+      if (changes) {
+        await BCMSRepo.apiKey.update(apiKey);
+      }
+    }
+  }
+}
 
 export function createBcmsFunctionModule(): Module {
   return {
     name: 'Function',
-    initialize(moduleConfig) {
-      let fns: BCMSFunction<unknown>[] = [];
-      const fs = useFS();
-      const objectUtil = useObjectUtility();
-      const stringUtil = useStringUtility();
-      const fnsPath = path.join(process.cwd(), 'functions');
-      fs.exist(fnsPath)
-        .then(async (result) => {
-          if (result) {
-            const fnNames = await fs.readdir(fnsPath);
-            for (let i = 0; i < fnNames.length; i++) {
-              const fnName = fnNames[i];
-              if (
-                fnName.endsWith('.js') ||
-                (!fnName.endsWith('.d.ts') && fnName.endsWith('.ts'))
-              ) {
-                const fnImport: {
-                  default: () => Promise<BCMSFunction<unknown>>;
-                } = await import(path.join(fnsPath, fnName));
-                const checkFn = objectUtil.compareWithSchema(
-                  { fn: fnImport.default },
-                  {
-                    fn: {
-                      __type: 'function',
-                      __required: true,
-                    },
-                  },
-                  fnName,
-                );
-                if (checkFn instanceof ObjectUtilityError) {
-                  moduleConfig.next(Error(checkFn.message));
-                  return;
-                }
-                const fn = await fnImport.default();
-                const checkObject = objectUtil.compareWithSchema(
-                  fn,
-                  BCMSFunctionSchema,
-                  fnName,
-                );
-                if (checkObject instanceof ObjectUtilityError) {
-                  moduleConfig.next(Error(checkObject.message));
-                  return;
-                }
-                fn.config.name = stringUtil.toSlug(fn.config.name);
-                if (fns.find((e) => e.config.name === fn.config.name)) {
-                  moduleConfig.next(
-                    Error(
-                      `Duplicate of "${fn.config.name}" function.` +
-                        ' This is not allowed.',
-                    ),
-                  );
-                  return;
-                }
-                fns.push(fn);
-              }
-            }
-          }
-          functionManager = {
-            clear() {
-              fns = [];
-            },
-            get(name) {
-              return fns.find((e) => e.config.name === name);
-            },
-            getAll() {
-              return fns;
-            },
-          };
-          moduleConfig.next();
-        })
-        .catch((error) => {
-          moduleConfig.next(error);
-        });
+    initialize({ next }) {
+      init()
+        .then(() => next())
+        .catch((err) => next(err));
     },
   };
 }
