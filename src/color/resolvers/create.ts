17f495dfb08e49cae622d292dafff4c6e0c5079e
Luka <lukagrujic80@gmail.com>
2022-01-19 13:42:11 +0100

    color create gql

--- src/color/resolvers/create.ts
+++ src/color/resolvers/create.ts
@@ -29,7 +29,7 @@ export const BCMSColorCreateResolver = createGraphqlResolver<
   type: GraphqlResolverType.MUTATION,
   args: {
     ...BCMSGraphqlSecurityArgs,
-    DataTransfer: 'BCMSColorCreateData!',
+    data: 'BCMSColorCreateData!',
   },
   async resolve({ accessToken, errorHandler, data }) {
     const jwt = securityVerifyJWT({
