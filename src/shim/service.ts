085f5fe4a1cd0a3faef87c2852f77f61196515ac
Bbanez <banezprod@gmail.com>
2023-05-08 15:30:31 +0200

    Update shim host resolve

--- src/shim/service.ts
+++ src/shim/service.ts
@@ -84,7 +84,10 @@ export async function createBcmsShimService(): Promise<void> {
   logger = useLogger({ name: 'Shim service' });
   http = createHttpClient({
     name: 'shimClient',
-    host: { name: 'bcms-shim', port: BCMSConfig.local ? '1279' : '3000' },
+    host: {
+      name: BCMSConfig.local ? 'bcms-shim' : '172.17.0.1',
+      port: BCMSConfig.local ? '1279' : '3000',
+    },
     basePath: '/shim',
   });
   const fs = useFS();
