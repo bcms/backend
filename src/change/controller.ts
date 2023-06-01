bb6422e4bc31ec8dfc7f4aae629a6d3fbc6fe749
Bbanez <banezprod@gmail.com>
2023-06-01 15:17:14 +0200

    Add change docs

--- src/change/controller.ts
+++ src/change/controller.ts
@@ -1,3 +1,4 @@
+import { bcmsCreateDocObject } from '@bcms/doc';
 import { BCMSRepo } from '@bcms/repo';
 import type { BCMSRouteProtectionJwtApiResult } from '@bcms/types';
 import { BCMSRouteProtection } from '@bcms/util';
@@ -9,43 +10,108 @@ import {
   JWTPermissionName,
   JWTRoleName,
 } from '@becomes/purple-cheetah-mod-jwt/types';
+import type { ObjectSchema } from '@becomes/purple-cheetah/types';
 
-interface GetInfoDataProp {
+export interface BCMSChangeGetInfoDataProp {
   count: number;
   lastChangeAt: number;
 }
+export const BCMSChangeGetInfoDataPropSchema: ObjectSchema = {
+  count: {
+    __type: 'number',
+    __required: true,
+  },
+  lastChangeAt: {
+    __type: 'number',
+    __required: true,
+  },
+};
 
-interface GetInfoData {
-  entry: GetInfoDataProp;
-  group: GetInfoDataProp;
-  color: GetInfoDataProp;
-  language: GetInfoDataProp;
-  media: GetInfoDataProp;
-  status: GetInfoDataProp;
-  tag: GetInfoDataProp;
-  templates: GetInfoDataProp;
-  widget: GetInfoDataProp;
+export interface BCMSChangeGetInfoData {
+  entry: BCMSChangeGetInfoDataProp;
+  group: BCMSChangeGetInfoDataProp;
+  color: BCMSChangeGetInfoDataProp;
+  language: BCMSChangeGetInfoDataProp;
+  media: BCMSChangeGetInfoDataProp;
+  status: BCMSChangeGetInfoDataProp;
+  tag: BCMSChangeGetInfoDataProp;
+  templates: BCMSChangeGetInfoDataProp;
+  widget: BCMSChangeGetInfoDataProp;
 }
+export const BCMSChangeGetInfoDataSchema: ObjectSchema = {
+  entry: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  group: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  color: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  language: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  media: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  status: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  tag: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  templates: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+  widget: {
+    __type: 'object',
+    __required: true,
+    __child: BCMSChangeGetInfoDataPropSchema,
+  },
+};
 
 export const BCMSChangeController = createController({
-  name: 'Change Controller',
+  name: 'Change',
   path: '/api/changes',
 
   methods() {
     return {
       getInfo: createControllerMethod<
         BCMSRouteProtectionJwtApiResult,
-        GetInfoData
+        BCMSChangeGetInfoData
       >({
         path: '/info',
         type: 'get',
+        doc: bcmsCreateDocObject({
+          summary: 'Get change information',
+          security: ['AccessToken', 'ApiKey'],
+          response: {
+            json: 'BCMSChangeGetInfoData',
+          },
+        }),
         preRequestHandler: BCMSRouteProtection.createJwtApiPreRequestHandler({
           roleNames: [JWTRoleName.ADMIN, JWTRoleName.USER],
           permissionName: JWTPermissionName.READ,
         }),
         async handler() {
           const changes = await BCMSRepo.change.findAll();
-          const output: GetInfoData = {
+          const output: BCMSChangeGetInfoData = {
             color: {
               count: 0,
               lastChangeAt: 0,
